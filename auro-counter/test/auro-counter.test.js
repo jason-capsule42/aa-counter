41cd69ac57888a182a576bfdc9ba43a15e6bb0d5
jason-capsule42 <jason@capsule42.com>
2021-01-24 00:44:23 -0800

    Strip default styling and associated karma test

--- auro-counter/test/auro-counter.test.js
+++ auro-counter/test/auro-counter.test.js
@@ -3,18 +3,9 @@ import sinon from 'sinon';
 import '../src/auro-counter.js';
 
 describe('auro-counter', () => {
-  it('sets the CSS class on auro-counter > div element', async () => {
-    const el = await fixture(html`
-      <auro-counter cssclass="testClass"></auro-counter>
-    `);
-
-    const div = el.shadowRoot.querySelector('div');
-    expect(div.className).to.equal('testClass');
-  });
-
   it('auro-counter is accessible', async () => {
     const el = await fixture(html`
-      <auro-counter cssclass="testClass"></auro-counter>
+      <auro-counter></auro-counter>
     `);
 
     await expect(el).to.be.accessible();
